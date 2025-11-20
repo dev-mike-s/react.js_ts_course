495d51989c45c0931c951477dddad3ae7fe0a7f9
Mike Stärke <michael.staerke@mail.de>
2025-11-20 17:11:34 +0100

    Config eslint, prettier, vite and add of content day3

---
+++ eslint.config.js
@@ -0,0 +1,36 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import { defineConfig, globalIgnores } from 'eslint/config';
+
+export default defineConfig([
+  globalIgnores(['dist']),
+  {
+    files: ['**/*.{js,jsx}'],
+    extends: [
+      js.configs.recommended,
+      reactHooks.configs.flat.recommended,
+      reactRefresh.configs.vite,
+      // Prettier-Regeln integrieren
+      'eslint:recommended',
+      'plugin:react/recommended',
+      'plugin:prettier/recommended',
+      'prettier',
+    ],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        ecmaFeatures: { jsx: true },
+        sourceType: 'module',
+      },
+    },
+    rules: {
+      'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
+      'prettier/prettier': 'error', // Prettier Fehler als ESLint Fehler anzeigen
+      'react/react-in-jsx-scope': 'off', // Ab React 17 nicht mehr notwendig
+    },
+  },
+]);
